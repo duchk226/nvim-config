61f4b7f83f3a30ca10482b51282eb74d4016d6c7
duc-hoang-kim <duchk@remitano.com>
2024-03-28 18:25:23 +0700

    first commit

---
+++ lua/duc-hoang/core/keymaps.lua
@@ -0,0 +1,81 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- general keymaps
+
+keymap.set("i", "jk", "<ESC>")
+keymap.set("i", "jj", "<ESC>:w<CR>")
+
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+keymap.set("n", "x", '"_x')
+
+keymap.set("n", "<leader>+", "<C-a>")
+keymap.set("n", "<leader>-", "<C-x>")
+
+keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sh", "<C-w>s")
+keymap.set("n", "<leader>se", "<C-w>=")
+keymap.set("n", "<leader>sx", ":close<CR>")
+
+keymap.set("n", "<leader>to", ":tabnew<CR>")
+keymap.set("n", "<leader>tx", ":tabclose<CR>")
+keymap.set("n", "<leader>]", ":tabn<CR>")
+keymap.set("n", "<leader>[", ":tabp<CR>")
+
+-- plugin keymap
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+
+keymap.set("n", "<leader>b", ":NvimTreeToggle<CR>")
+
+keymap.set("n", "<leader>b", ":NvimTreeToggle<CR>")
+
+keymap.set("n", "<leader>]", ":tabn<CR>")
+
+keymap.set("n", "<leader>l", ":NvimTreeFindFile<CR>")
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")  -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")   -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")     -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")   -- list available help tags
+
+-- gitblame
+keymap.set("n", "<leader>gb", ":GitBlameToggle<CR>")
+keymap.set("n", "<leader>gl", ":GitBlameOpenFileURL<CR>")
+
+-- window resize
+keymap.set("n", "<leader>rh", "<cmd>vertical resize -5<cr>")
+keymap.set("n", "<leader>rl", "<cmd>vertical resize +5<cr>")
+keymap.set("n", "<leader>rj", "<cmd>horizontal resize -5<cr>")
+keymap.set("n", "<leader>rk", "<cmd>horizontal resize +5<cr>")
+
+keymap.set("n", "<C-f>", vim.lsp.buf.format)
+
+keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+
+-- git conflict
+keymap.set("n", "<leader>c,", ":GitConflictListQf<CR>")
+keymap.set("n", "<leader>cn", ":GitConflictNextConflict<CR>")
+keymap.set("n", "<leader>cm", ":GitConflictPrevConflict<CR>")
+keymap.set("n", "<leader>ch", ":GitConflictChooseOurs<CR>")
+keymap.set("n", "<leader>cj", ":GitConflictChooseTheirs<CR>")
+keymap.set("n", "<leader>ck", ":GitConflictChooseBoth<CR>")
+keymap.set("n", "<leader>cl", ":GitConflictChooseNone<CR>")
+
+-- reset vim input buffers
+keymap.set("n", "<leader>co", ":bufdo e!<CR>")
+
+keymap.set("n", "<leader>y", ":let @+ = expand(\"%\")<CR>")
+keymap.set("n", "<leader>y", ":let @+ = expand(\"%:p\")<CR>")
+
+local o = vim.o
+
+o.expandtab = true
+o.smartindent = true
+o.tabstop = 2
+o.shiftwidth = 2
---
+++ lua/duc-hoang/plugins/nvim-tree.lua
@@ -0,0 +1,24 @@
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then
+  return
+end
+
+vim.g.loaded = 1 
+vim.g.loaded_netrwPlugin = 1
+
+nvimtree.setup({
+ git = {
+  enable = true,
+  ignore = false,
+  timeout = 500
+ } 
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd "quit"
+    end
+  end
+})
